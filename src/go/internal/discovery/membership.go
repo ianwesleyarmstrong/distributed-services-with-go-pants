0872d309a385b07fc9de5306ea308c5eea1bddbe
Ian Armstrong <ianwesleyarmstrong@gmail.com>
2023-08-01 22:21:30 -0400

    implement distributed log using raft

--- src/go/internal/discovery/membership.go
+++ src/go/internal/discovery/membership.go
@@ -4,6 +4,7 @@ import (
 	"go.uber.org/zap"
 	"net"
 
+	"github.com/hashicorp/raft"
 	"github.com/hashicorp/serf/serf"
 )
 
@@ -118,7 +119,11 @@ func (m *Membership) handleLeave(member serf.Member) {
 }
 
 func (m *Membership) logError(err error, msg string, member serf.Member) {
-	m.logger.Error(
+	log := m.logger.Error
+	if err == raft.ErrNotLeader {
+		log = m.logger.Debug
+	}
+	log(
 		msg,
 		zap.Error(err),
 		zap.String("name", member.Name),
